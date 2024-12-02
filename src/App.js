7019f201a4779f16ddb5fe24c4fe2e308aa84a14
mizanur090148 <mizanur090148@gmail.com>
2024-12-02 22:18:56 +0600

    edit app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is react app
+          This is CI/CD app
         </p>
         <a
           className="App-link"
